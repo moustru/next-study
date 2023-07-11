d2785055a4edb904de2bebb1188feee7bf57afff
Eugene M <xexyz@yandex.ru>
2023-07-12 01:01:03 +0300

    Добавил React Hook Form

--- src/config/layout/Footer/index.tsx
+++ src/config/layout/Footer/index.tsx
@@ -1,9 +1,33 @@
+import { useForm, SubmitHandler } from 'react-hook-form';
 import css from './index.module.css';
 
+type Inputs = {
+	example: string;
+	exampleRequired: string;
+};
+
 export const Footer = () => {
+	const {
+		register,
+		handleSubmit,
+		watch,
+		formState: { errors },
+	} = useForm<Inputs>();
+	const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
 	return (
 		<footer className={css.footer}>
-			<p>Hello</p>
+			<form onSubmit={handleSubmit(onSubmit)}>
+				{/* register your input into the hook by invoking the "register" function */}
+				<input defaultValue="test" {...register('example')} />
+
+				{/* include validation with required or other standard HTML validation rules */}
+				<input {...register('exampleRequired', { required: true })} />
+				{/* errors will return when field validation fails  */}
+				{errors.exampleRequired && <span>This field is required</span>}
+
+				<input type="submit" />
+			</form>
 		</footer>
 	);
 };
