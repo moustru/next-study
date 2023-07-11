48c51faf8f2f4abbf69e67b4ac1f125bbf20827b
Eugene M <xexyz@yandex.ru>
2023-07-12 00:38:00 +0300

    Добавил конфиг

--- src/config/theme/index.ts
+++ src/config/theme/index.ts
@@ -1,5 +1,8 @@
 import { extendTheme } from '@chakra-ui/react';
 
+import colors from './colors';
+import typography from './typography';
+
 // import { Button } from './components/Button';
 
 const VibeLabTheme = extendTheme({
@@ -7,29 +10,17 @@ const VibeLabTheme = extendTheme({
 	components: {
 		// ...Button,
 	},
-	//------  components end  ------//
 
-	//------  colors tokens start  ------//
-	semanticTokens: {
-		colors: {
-			'text-primary': '#141516',
-			'text-secondary': '#2C2A2A',
-			'text-tertiary': '#7C858D',
-			'text-quaternary': '#A0AEC0',
-			'text-accent': 'blue.600',
-			'text-on-accent': 'white',
-			// success: 'green.500',
-			// primary: {
-			//   default: 'red.500',
-			//   _dark: 'red.400',
-			// },
-			// secondary: {
-			//   default: 'red.800',
-			//   _dark: 'red.700',
-			// },
+	styles: {
+		global: {
+			body: {
+				// ...
+			},
 		},
 	},
-	//------  colors tokens end  ------//
+
+	colors,
+	typography,
 });
 
 export default VibeLabTheme;
