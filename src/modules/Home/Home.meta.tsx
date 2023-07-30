51dcbbae28693922b419f619fc4b56d755463bd6
Eugene M <xexyz@yandex.ru>
2023-07-30 19:10:19 +0300

    Добавил мету в shared

--- src/modules/Home/Home.meta.tsx
+++ src/modules/Home/Home.meta.tsx
@@ -1,7 +1,8 @@
+import { MetaInfo } from '@/shared/components/MetaInfo';
 import Head from 'next/head';
+
 export const HomePageMeta = () => (
 	<Head>
-		<title>Vibe Lab – главная</title>
-		<meta name="viewport" content="width=device-width,initial-scale=1" />
+		<MetaInfo title="Vibe Lab – главная" />
 	</Head>
 );
