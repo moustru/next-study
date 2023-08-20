d19e278e049243969a73c6d5779f970be30e8cc7
Albert Rybalko <albert.rybalko@chulakov.ru>
2023-08-20 21:12:41 +0300

    feat: update animation of appiaring articales card

--- src/modules/Home/sections/SectionNews/index.tsx
+++ src/modules/Home/sections/SectionNews/index.tsx
@@ -3,7 +3,7 @@ import { Flex } from '@chakra-ui/react';
 import { SectionTemplate } from '@/modules/Common/sections/SectionTemplate';
 import { NewsItem } from '@/shared/components/NewsItem';
 
-// import { news } from './mocks';
+import { news as news2 } from './mocks';
 
 type SectionNewsModel = {
 	news?: {
@@ -18,7 +18,7 @@ export const SectionNews = ({ news = [] }: SectionNewsModel) => {
 	return (
 		<SectionTemplate title="Новости">
 			<Flex gap={{ lg: 8, xs: 4 }} wrap={{ lg: 'nowrap', xs: 'wrap' }}>
-				{news.map((newsItem, i) => (
+				{news2.map((newsItem, i) => (
 					<NewsItem {...newsItem} key={newsItem.title + i} />
 				))}
 			</Flex>
