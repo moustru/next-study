cf59803e1d3933f5a4745cd1f2fe8397936ac96b
Albert Rybalko <albert.rybalko@chulakov.ru>
2023-08-27 18:26:27 +0300

    feat: adaptive for main page

--- src/shared/components/MovingArrow/index.tsx
+++ src/shared/components/MovingArrow/index.tsx
@@ -1,6 +1,7 @@
 import { motion } from 'framer-motion';
 
 import css from './index.module.css';
+import { useMediaQuery } from '@chakra-ui/react';
 
 export const MovingArrow = ({
 	isHoverOnParent,
@@ -10,13 +11,18 @@ export const MovingArrow = ({
 	isDark?: boolean;
 }) => {
 	const coloring = isDark ? 'light' : 'dark';
+
+	const [isMobile] = useMediaQuery('(max-width: 768px)', {
+		ssr: true,
+		fallback: true,
+	});
 	return (
 		<div data-color={coloring} className={css.movingWay}>
 			<motion.div
 				className={css.arrow}
 				data-color={coloring}
 				initial={{ x: '-10%' }}
-				animate={{ x: isHoverOnParent ? '100%' : '5%' }}
+				animate={{ x: !isMobile && isHoverOnParent ? '100%' : '5%' }}
 				transition={{ duration: 0.3, cubicBezier: '0.83, 0, 0.17, 1' }}
 			/>
 		</div>
